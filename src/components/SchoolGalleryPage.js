60b81de131b40135e3459ffd2b67fcd95e331c5d
soumik sarkar <saikatsarkar502@gmail.com>
2022-06-17 23:34:57 +0530

    minor changes

--- src/components/SchoolGalleryPage.js
+++ src/components/SchoolGalleryPage.js
@@ -9,21 +9,23 @@ function SchoolGalleryPage() {
 
         <div>
             <HomeBar />
-            <div className="con1">
-                <img src="./images/hcover.jpeg"></img>
-                
-            </div>
-            <div className="pos">
-                <img src="./images/logo1.jpg"></img>
-                <h2><b>Holy Mother English School</b></h2>
-                <h3>@school name</h3>
-                <button>Message</button>
+            <div style={{ margin: "0 75px", width: "100%", }}>
+                <div className="con1">
+                    <img src="./images/hcover.jpeg"></img>
 
-            </div>
-            <div className="con2">
-                <hr />
-                <SideNavBar />
+                </div>
+                <div className="pos">
+                    <img src="./images/logo1.jpg"></img>
+                    <h2><b>Holy Mother English School</b></h2>
+                    <h3>@school name</h3>
+                    <button>Message</button>
 
+                </div>
+                <div className="con2">
+                    <hr />
+                    <SideNavBar />
+
+                </div>
             </div>
             <div className="con3">
                 <h4><b>Gallery</b></h4>
@@ -34,15 +36,15 @@ function SchoolGalleryPage() {
                 <button>Albums</button>
             </div>
             <div className="con5">
-            <img src="./images/2.jpg"></img>
-            <img src="./images/bg1.jpg"></img>
-            <img src="./images/prize.jpg"></img>
-            <img src="./images/game.jpg"></img>
-            <img src="./images/song.jpg"></img>
-            <img src="./images/2.jpg"></img>
-            <img src="./images/xmas.jpg"></img>
-            <img src="./images/trip.jpg"></img>
-            <img src="./images/prize.jpg"></img>
+                <img src="./images/2.jpg"></img>
+                <img src="./images/bg1.jpg"></img>
+                <img src="./images/prize.jpg"></img>
+                <img src="./images/game.jpg"></img>
+                <img src="./images/song.jpg"></img>
+                <img src="./images/2.jpg"></img>
+                <img src="./images/xmas.jpg"></img>
+                <img src="./images/trip.jpg"></img>
+                <img src="./images/prize.jpg"></img>
 
             </div>
         </div>
