c71d988186babb0641d33b1c54b5999b083ffc55
soumik sarkar <saikatsarkar502@gmail.com>
2022-06-17 23:42:14 +0530

    basic changes

--- src/components/HomeBar.js
+++ src/components/HomeBar.js
@@ -6,24 +6,20 @@ import AddAlertIcon from '@material-ui/icons/AddAlert';
 function HomeBar() {
     return (
 
-         
+
         <div class="nav">
             <span>
-                
-                
                 <a href="#">You</a>
                 <a href="#">Notifications</a>
                 <a href="#">Institutes</a>
                 <a href="#">School Buddies</a>
-                <a  href='#' className='pro' img src='./images/promoralogo.png'></a>
-                
-                
-            
+                <a href='#' className='pro' img src='./images/promoralogo.png'></a>
+
             </span>
-            </div>
-        
-            
-        
+        </div>
+
+
+
     )
 
 }
